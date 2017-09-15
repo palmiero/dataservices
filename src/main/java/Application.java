fe6d19304c8a10461d148f88250b50a6149d8923
pmpalmer <pmpalmer@acnc-md.email>
2017-09-15 09:10:37 -0400

    added Application class and modded build.gradle a bit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
